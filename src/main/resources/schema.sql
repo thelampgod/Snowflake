55c795794b1a08d2a3db864387c42e173c21969e
lamp <virtual@riseup.net>
2024-06-10 21:26:03 +0200

    Fix group_users entries not deleting when the group/user was deleted

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,15 +6,18 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS groups (
     id INTEGER PRIMARY KEY,
     name COLLATE NOCASE NOT NULL,
-    owner_id INTEGER NOT NULL,
-
-    -- Delete deleted group reference in users table on group delete
-    FOREIGN KEY(id) REFERENCES group_users(group_id) ON DELETE CASCADE
+    owner_id INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS group_users (
     user_id INTEGER NOT NULL,
     group_id INTEGER NOT NULL,
 
-    UNIQUE(user_id, group_id)
+    UNIQUE(user_id, group_id),
+
+    -- Delete rows when the referenced group is deleted
+    FOREIGN KEY(group_id) REFERENCES groups(id) ON DELETE CASCADE,
+
+    -- Delete rows when the referenced user is deleted
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
