a542d66273ceb21b37d562e7ed38ae596c8f5b37
lamp <virtual@riseup.net>
2024-06-25 19:40:13 +0200

    Limit speed of packets if they are identical to last

--- Client/src/main/java/com/github/thelampgod/snow/waypoints/share/WaypointSharer.java
+++ Client/src/main/java/com/github/thelampgod/snow/waypoints/share/WaypointSharer.java
@@ -5,11 +5,17 @@ import com.github.thelampgod.snow.groups.Group;
 import com.github.thelampgod.snow.packets.impl.EncryptedDataPacket;
 import com.github.thelampgod.snow.packets.impl.LocationPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.github.thelampgod.snow.Helper.mc;
 
 public class WaypointSharer {
     private Group selectedGroup;
+
+    private Vec3d lastPos;
+    private long lastPacketSent;
 
     public void onLocationSend(PlayerMoveC2SPacket packet) {
         if (mc.world == null) return;
@@ -20,10 +26,17 @@ public class WaypointSharer {
         double z = packet.getZ(0);
         byte dimension = (byte) (mc.world.getDimension().coordinateScale() == 8.0d ? -1 : 0);
 
+        Vec3d pos = new Vec3d(x, y, z);
+        if (pos.equals(lastPos) && System.currentTimeMillis() < lastPacketSent + TimeUnit.SECONDS.toMillis(1)) {
+            return;
+        }
+
+        lastPos = pos;
         try {
             Snow.getServerManager().sendPacket(
                     new EncryptedDataPacket.Group(selectedGroup.getId(),
                             new LocationPacket(selectedGroup.getId(), x, y, z, dimension)));
+            lastPacketSent = System.currentTimeMillis();
         } catch (Exception e) {
             Snow.instance.getLog().error("Error encrypting location: " + e.getMessage(), e);
         }
@@ -32,6 +45,7 @@ public class WaypointSharer {
     public void select(Group group) {
         selectedGroup = group;
     }
+
     public void clear() {
         selectedGroup = null;
     }
